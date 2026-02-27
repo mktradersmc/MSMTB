70043aafaae701fde918d9f9fb92e8edc0906ad9
Michael <michael.mueller.germany@gmail.com>
2026-02-27 17:31:25 +0100

    fix(pm2): default nextjs server.js to production mode to avoid rust compiler dev-mode panic on ceph vms

--- src/trading-cockpit/server.js
+++ src/trading-cockpit/server.js
@@ -22,7 +22,7 @@ const useSSL = sysConfig?.backend?.useSSL === true;
 const port = sysConfig?.frontend?.port || 443;
 const pfxPassword = sysConfig?.backend?.pfxPassword || 'cockpit';
 
-const dev = process.env.NODE_ENV !== 'production';
+const dev = process.env.NODE_ENV === 'development'; // Standard: Production! Umgeht Rust/SWC Dev-Compiler
 const app = next({ dev, hostname: '0.0.0.0', port });
 const handle = app.getRequestHandler();
 
